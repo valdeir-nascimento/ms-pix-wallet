f21e91d0c6e59310403d780f5a3c8927ffa0acda
Valdeir Nascimento <valdeirnascimento.dev@gmail.com>
2025-11-26 21:22:12 -0300

    feature: configurations

---
+++ src/main/java/br/com/pix/wallet/infrastructure/config/ObjectMapperConfig.java
@@ -0,0 +1,17 @@
+package br.com.pix.wallet.infrastructure.config;
+
+import br.com.pix.wallet.infrastructure.config.json.Json;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Json.mapper();
+    }
+
+}
---
+++ src/main/java/br/com/pix/wallet/infrastructure/config/WebServerConfig.java
@@ -0,0 +1,10 @@
+package br.com.pix.wallet.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.pix.wallet")
+public class WebServerConfig {
+
+}
