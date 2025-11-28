588d73ef5defe77af28529f4f2254ea7eb9555af
Valdeir Nascimento <valdeirnascimento.dev@gmail.com>
2025-11-27 21:52:28 -0300

    test: unit test

--- src/test/java/br/com/pix/wallet/application/wallet/create/CreateWalletUseCaseImplTest.java
+++ src/test/java/br/com/pix/wallet/application/wallet/create/CreateWalletUseCaseImplTest.java
@@ -1,21 +1,28 @@
 package br.com.pix.wallet.application.wallet.create;
 
-import br.com.pix.wallet.domain.exception.DomainException;
-import br.com.pix.wallet.domain.wallet.Wallet;
-import br.com.pix.wallet.domain.wallet.WalletGateway;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.pix.wallet.domain.exception.DomainException;
+import br.com.pix.wallet.domain.wallet.Wallet;
+import br.com.pix.wallet.domain.wallet.WalletGateway;
 
 @ExtendWith(MockitoExtension.class)
 class CreateWalletUseCaseImplTest {
@@ -64,5 +71,24 @@ class CreateWalletUseCaseImplTest {
         assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
 
         verify(walletGateway, times(0)).save(any());
+    }
+
+    @Test
+    void givenExistingOwnerId_whenCallsExecute_thenShouldThrowDomainException() {
+        // given
+        final var expectedOwnerId = UUID.randomUUID().toString();
+        final var expectedErrorMessage = "Owner ID already has a wallet";
+        final var command = CreateWalletCommand.with(expectedOwnerId);
+
+        when(walletGateway.existsByOwnerId(expectedOwnerId)).thenReturn(true);
+
+        // when
+        final var actualException = assertThrows(DomainException.class, () -> useCase.execute(command));
+
+        // then
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(walletGateway, times(1)).existsByOwnerId(expectedOwnerId);
+        verify(walletGateway, never()).save(any());
     }
 }
